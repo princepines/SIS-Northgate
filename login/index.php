465e6badc37677ef6903d08ddf436a662875ca07
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-03-20 07:49:33 +0000

    added functionality in login, added profile page, navigation bars, etc.

--- login/index.php
+++ login/index.php
@@ -1,9 +1,42 @@
+<?php
+   $loc = str_replace("login", "", __DIR__);
+   /*
+   $profile = $loc . 'profile.php';
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: $profile");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+   */
+?>
+
 <html>
 <?php 
-    $loc = str_replace("login", "", __DIR__);
     require $loc . 'head.php';
 ?>
- <body>
+<title>SIS Login</title>
+</head>
+ <body style="background-color: #4a89dc !important;">
     <div class="container">
         <div class="row">
             <div class="col">
---
+++ nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#" style="font-family: Kanit, sans-serif;">Student IS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="/">Home</a>
+      <a class="nav-item nav-link" href="subjects.php">Subjects</a>
+      <a class="nav-item nav-link" href="payments.php">Payments</a>
+    </div>
+  </div>
+</nav>
